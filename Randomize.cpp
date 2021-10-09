9dadab1fea9ef6ecaf3c364e688914e24d0d1050
QuitePixel <silentpxl@gmail.com>
2021-10-09 18:06:17 +0300

    Create Randomize.cpp

---
+++ Randomize.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	int const SIZE = 10;
+	int A[10];
+	bool AlreadyThere;
+	for ( int i = 0; i < SIZE; )
+	{
+		AlreadyThere = false;
+		int newRandomValue = rand() % 20;
+		for ( int j = 0; j < i ; j++)
+		{
+			if (A[j] == newRandomValue)
+			{
+				AlreadyThere = true;
+				break;
+			}
+		
+		
+		}
+		if (!AlreadyThere)
+		{
+			A[i] = newRandomValue;
+			i++;
+		}
+		
+		
+		
+		
+					
+		
+		
+
+	}
+
+	for ( int i = 0; i < SIZE; i++)
+	{
+		cout << "\n A[" << i << "]=" << A[i];
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
