6f4dc3bc2707979a6847d36447a783ce222199c4
QuitePixel <silentpxl@gmail.com>
2021-10-09 18:25:29 +0300

    Update Randomize.cpp

--- Randomize.cpp
+++ Randomize.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+﻿﻿#include <iostream>
 #include <ctime>
 using namespace std;
 int main()
@@ -6,56 +6,56 @@ int main()
 	setlocale(LC_ALL, "ru");
 	srand(time(NULL));
 	int const SIZE = 10;
-	int A[10];
+	int A[SIZE];
 	bool AlreadyThere;
-	for ( int i = 0; i < SIZE; )
+	for (int i = 0; i < SIZE; )
 	{
 		AlreadyThere = false;
 		int newRandomValue = rand() % 20;
-		for ( int j = 0; j < i ; j++)
+		for (int j = 0; j < i; j++)
 		{
 			if (A[j] == newRandomValue)
 			{
 				AlreadyThere = true;
 				break;
 			}
-		
-		
+
+
 		}
 		if (!AlreadyThere)
 		{
 			A[i] = newRandomValue;
 			i++;
 		}
-		
-		
-		
-		
-					
-		
-		
+
+
+
+
+
+
+
 
 	}
 
-	for ( int i = 0; i < SIZE; i++)
+	for (int i = 0; i < SIZE; i++)
 	{
 		cout << "\n A[" << i << "]=" << A[i];
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	return 0;
 }
